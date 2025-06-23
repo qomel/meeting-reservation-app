cf7ecc0aee9f664d0640ccc3347b9208761e8518
qomel <80347310+qomel@users.noreply.github.com>
2025-06-23 15:46:34 +0200

    Dodanie ponowanie backendu

--- src/components/RequireAuth.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-export default function RequireAuth() {
-  const user = localStorage.getItem('user');
-
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
-}
